949fe1ce9cfeaee58f0e23572412d20a366d0b1c
Peter Raback <peter.raback@csc.fi>
2020-09-17 03:38:05 +0300

    Squashed commit of the following:
    commit 40f582b32825f413615a34202a863b16011828a0
    Merge: c785f20 2c54399
    Author: Peter Raback <peter.raback@csc.fi>
    Date:   Thu Sep 17 03:35:05 2020 +0300
    
        Merge branch 'devel' into EG_merge
    
    commit c785f20d8708b8cd64d98b47de4bb69dfe6cedb0
    Merge: a23814c d7aaeb2
    Author: Peter Raback <peter.raback@csc.fi>
    Date:   Wed Sep 16 21:26:04 2020 +0300
    
        Merge branch 'EG_compare' into EG_merge
    
    commit d7aaeb215fb3a5fe5e81b3537180a74bee1567c8
    Merge: 854057a 4476e0f
    Author: Peter Raback <peter.raback@csc.fi>
    Date:   Wed Sep 16 20:02:11 2020 +0300
    
        Merge branch 'devel' into EG_compare
    
    commit a23814c420a41245efdbe934185050f208612eea
    Author: Peter Raback <peter.raback@csc.fi>
    Date:   Wed Sep 16 16:32:03 2020 +0300
    
        Merge new ElmerGrid with its plugin
    
    commit 854057a9c9c53c3516f534de9c71ae57ae8be5af
    Author: Peter Raback <peter.raback@csc.fi>
    Date:   Wed Sep 16 16:24:46 2020 +0300
    
        Remove some obsolite stuff
    
    commit 69c27921e63cc2bf6a01aac186bb2a19b83981a5
    Author: Peter Raback <peter.raback@csc.fi>
    Date:   Wed Sep 16 13:58:00 2020 +0300
    
        Minos restructuring for ElmerGrid plugin interoperability
    
    commit df55c0aa1a7d8675b5d597c782c5671335c7eed1
    Author: Peter Raback <peter.raback@csc.fi>
    Date:   Tue Sep 15 18:01:11 2020 +0300
    
        Move EchoFile routine to extra
    
    commit ad8f3f9947adcd22673a31a3a48d2effed5f41f2
    Author: Peter Raback <peter.raback@csc.fi>
    Date:   Tue Sep 15 18:00:15 2020 +0300
    
        Move instructions away from main
    
    commit ae98027551a278681acbd0568ef9e735eed0d5c0
    Author: Peter Raback <peter.raback@csc.fi>
    Date:   Tue Sep 15 17:08:22 2020 +0300
    
        Remove instructions from main program module
    
    commit 09878e8beb2da438415b2c2d71e2220272ca78c9
    Author: Peter Raback <peter.raback@csc.fi>
    Date:   Tue Sep 15 17:06:45 2020 +0300
    
        Harmonize ElmerGrid with respect to ElmerGUI plugin

---
+++ elmergrid/src/grid2gui.sh
@@ -0,0 +1,5 @@
+cp -f egconvert.c ../../ElmerGUI/Application/plugins/egconvert.cpp
+cp -f egmesh.c ../../ElmerGUI/Application/plugins/egmesh.cpp
+cp -f egnative.c ../../ElmerGUI/Application/plugins/egnative.cpp
+cp -f egutils.c ../../ElmerGUI/Application/plugins/egutils.cpp
+cp -f egconvert.h egmesh.h egnative.h egutils.h egtypes.h ../../ElmerGUI/Application/plugins 
